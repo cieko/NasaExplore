69beeebcce5918581233615617206e2b307c811f
Cieko <110197023+cieko@users.noreply.github.com>
2025-01-25 21:06:14 +0530

    Mongodb cloud connected

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -8,7 +8,9 @@ const launch = {
   rocket: 'Explorer IS1',
   launchDate: new Date('December 27, 2030'),
   target: 'Kepler-442 b',
-  customer: ['ZTM', 'NASA']
+  customer: ['ZTM', 'NASA'],
+  upcoming: true,
+  success: true
 };
 
 launches.set(launch.flightNumber, launch) // indexing using flightNumber
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const http = require('http')
 
+const mongoose = require('mongoose')
+
 const app = require('./app');
 
 const {
@@ -8,9 +10,20 @@ const {
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = 'mongodb+srv://cieko:nj93GKxJ7KShxOeI@nasackuster.ak6hc.mongodb.net/nasa?retryWrites=true&w=majority&appName=NasaCkuster'
+
 const server = http.createServer(app)
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!')
+})
+
+mongoose.connection.on('error', (err) => {
+  console.error(err)
+})
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL)
   await loadPlanetsData();
 
   server.listen(PORT, () => {
