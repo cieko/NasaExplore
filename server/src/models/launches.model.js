b22d13f702b1fd908ece7524a4d9213399c1d9c4
Cieko <110197023+cieko@users.noreply.github.com>
2025-02-06 23:14:31 +0530

    populating with spacex data

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,24 +1,97 @@
 const launchesDatabase = require('./launches.mongo')
 const planets = require('./planets.mongo')
+const axios = require('axios')
 
 const DEFAULT_FLIGHT_NUMBER = 100;
-const launches = new Map()
+// const launches = new Map()
 
 // let latestFlightNumber = 100 // last flight Number that was scheduled
 
 const launch = {
-  flightNumber: 100,
-  mission: 'Kepler Exploration X',
-  rocket: 'Explorer IS1',
-  launchDate: new Date('December 27, 2030'),
-  target: 'Kepler-442 b',
-  customers: ['ZTM', 'NASA'],
-  upcoming: true,
-  success: true
+  flightNumber: 100, // flight_number
+  mission: 'Kepler Exploration X',  // name
+  rocket: 'Explorer IS1',  // rocket.name
+  launchDate: new Date('December 27, 2030'), // date_local
+  target: 'Kepler-442 b', // NA
+  customers: ['ZTM', 'NASA'], // payload.customers
+  upcoming: true, // upcoming
+  success: true // success
 };
 
 // launches.set(launch.flightNumber, launch) // indexing using flightNumber
 saveLaunch(launch)
+
+const SPACEX_API_URL = "https://api.spacexdata.com/v4/launches/query"
+
+async function populateLaunches() {
+  const response = await axios.post(SPACEX_API_URL, {
+    query: {},
+    options: {
+      pagination: false,
+      populate: [
+        {
+          path: 'rocket',
+          select: {
+            name: 1
+          }
+        },
+        {
+          path: 'payloads',
+          select: {
+            'customer': 1
+          }
+        }
+      ]
+    }
+  })
+
+  if (response.status !== 200) {
+    console.log('Problem downloading launch data')
+    throw new Error('Launch data download failed')
+  }
+
+  const launchDocs = response.data.docs;
+
+  for (const launchDoc of launchDocs) {
+    const payloads = launchDoc['payloads']
+    const customer = payloads.flatMap((payload) => {
+      return payload['id']
+    })
+
+    const launch = {
+      flightNumber: launchDoc['flight_number'],
+      mission: launchDoc['name'],
+      rocket: launchDoc['rocket']['name'],
+      launchDate: launchDoc['date_local'],
+      upcoming: launchDoc['upcoming'],
+      success: launchDoc['success'],
+      customer
+    }
+
+    console.log(launch)
+
+    // TODO: populate launches collection
+    await saveLaunch(launch)
+  }
+}
+
+async function loadLaunchData() {
+  const firstLaunch = await findLaunch({
+    flightNumber: 1,
+    rocket: 'Falcon 1',
+    mission: 'FalconSat'
+  })
+
+  if (firstLaunch) {
+    console.log('Launch data already loaded')
+  } else {
+    await populateLaunches()
+  }
+}
+
+async function findLaunch(filter) {
+  return await launchesDatabase.findOne(filter)
+}
 
 async function existsLaunchWithId(launchId) {
   // return launches.has(launchId)
@@ -46,13 +119,13 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target
-  })
+  // const planet = await planets.findOne({
+  //   keplerName: launch.target
+  // })
 
-  if (!planet) {
-    throw new Error('No matching planet was found') // built in node error class
-  }
+  // if (!planet) {
+  //   throw new Error('No matching planet was found') // built in node error class
+  // }
 
   await launchesDatabase.findOneAndUpdate({
     flightNumber: launch.flightNumber, // check for the document already exists or not
@@ -61,6 +134,13 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target
+  })
+
+  if (!planet) {
+    throw new Error('No matching planet was found') // built in node error class
+  }
 
   const newFlightNumber = await getLatestFlightNumber() + 1;
 
@@ -108,6 +188,7 @@ async function abortLaunchById(launchId) {
 }
 
 module.exports = {
+  loadLaunchData,
   existsLaunchWithId,
   getAllLaunches,
   addNewLaunch: scheduleNewLaunch,
